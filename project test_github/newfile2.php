88b05aa83e2f4cd792c71c0339b9bd021a5d1a63
AlexanderK013 <supportk130@gmail.com>
2022-02-08 19:28:01 +0300

    newfile2.phpL3

--- project test_github/newfile2.php
+++ project test_github/newfile2.php
@@ -2,6 +2,6 @@
 <<<<<<< HEAD
 //newfile2.php//newfile2.php
 //newfile2.php//newfile2.php
-=======
+==============
 //newfile2.php//newfile2.php//////////
 >>>>>>> branch 'main' of https://github.com/AlexanderK013/repository2.git
