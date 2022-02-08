125074ada8d80da30f6b1f11b1f47a9929f300ea
AlexanderK013 <supportk130@gmail.com>
2022-02-08 19:45:49 +0300

    newfile2.phpL4_corrected

--- project test_github/newfile2.php
+++ project test_github/newfile2.php
@@ -2,7 +2,7 @@
 //<<<<<<< HEAD
 //newfile2.php//newfile2.php
 //newfile2.php//newfile2.php
-============================
+//============================
 //newfile2.php//newfile2.php//////////
->>>>>>> branch 'main' of https://github.com/AlexanderK013/repository2.git
-==============
+//>>>>>>> branch 'main' of https://github.com/AlexanderK013/repository2.git
+//==============
