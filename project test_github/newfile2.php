4eb6c6102d802d3ffddf2cbd62c27892f14e139c
AlexanderK013 <supportk130@gmail.com>
2022-02-09 20:25:45 +0300

    conflict removed

--- project test_github/newfile2.php
+++ project test_github/newfile2.php
@@ -5,9 +5,5 @@
 //============================
 //newfile2.php//newfile2.php//////////
 //>>>>>>> branch 'main' of https://github.com/AlexanderK013/repository2.git
-<<<<<<< HEAD
 //==============branch 'main'
-//==============branch 'main'
-=======
-//============== branch branch BRANCH_fix
->>>>>>> refs/heads/BRANCH_fix
+//==============branch 'main'  refs/heads/BRANCH_fix
